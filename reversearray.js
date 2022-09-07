29ee6cd4759eba57e56b165c7a9a07d2bbe3c7d4
mobcoder-preety-gupta <preety.gupta@mobcoder.com>
2022-09-07 13:11:27 +0530

    get reverse of array

--- reversearray.js
+++ reversearray.js
@@ -1,15 +1,15 @@
 // 3 Reverse the array
 // approach 1: using reverse method
-let arr=[1,2,3,3,2]
+let arr = [1, 2, 3, 3, 2]
 
-arr=arr.reverse()
+arr = arr.reverse()
 console.log(arr)
 
 // approach 2: using loop
-let arr1=[1,2,3,3,2]
-let arr2=[]
-for(let i=arr1.length;i>0;i--){
-    arr2.push(arr1[i-1])
+let arr1 = [1, 2, 3, 3, 2]
+let arr2 = []
+for (let i = arr1.length; i > 0; i--) {
+    arr2.push(arr1[i - 1])
 }
 
 console.log(arr2)
