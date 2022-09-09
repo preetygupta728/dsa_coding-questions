bbb665eaa96d908cec232d55efed2a024894658a
mobcoder-preety-gupta <preety.gupta@mobcoder.com>
2022-09-09 15:02:38 +0530

    find duplicate in an array of N+1 Integers

--- getduplicateelement.js
+++ getduplicateelement.js
@@ -1,8 +1,4 @@
-12. Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-    There is only one repeated number in nums, return this repeated number.
-    You must solve the problem without modifying the array nums and uses only constant extra space.
-
-*/
+// find duplicate in an array of N+1 Integers
 let nums = [3,1,3,4,2]
 
 let a=[]
