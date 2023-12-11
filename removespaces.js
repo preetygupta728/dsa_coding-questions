d4240bf9a38f8283753c03e957ad6951ff330aa5
mobcoder-preety-gupta <preety.gupta@mobcoder.com>
2023-12-11 14:44:34 +0530

    remove white space

---
+++ removespaces.js
@@ -0,0 +1,10 @@
+let str="I ng, sghdvshndb"
+console.log(str.replace(/\s/g,""))
+//
+let spli=str.split(" ")
+console.log(spli.join(''))
+// let spliss=''
+// for(let i=0;i<spli.length;i++){
+// spliss=spliss+spli[i]
+// }
+// console.log(spliss)
